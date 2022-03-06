8f4a32f0ca55815740b362c16f666c33f9fb57d4
Lucas Souza <lucasgabriel3490@gmail.com>
2022-03-05 23:15:14 -0300

    Comentando o código

--- hello.asm
+++ hello.asm
@@ -4,13 +4,13 @@ section .data
 	msg db "Hello, World!",10
 section .text
 _start:
-	mov eax, 1
-	mov edi, 1
-	mov esi, msg
-	mov edx, 14
-	syscall
+	mov eax, 1	; Armazena o valor da chamada de write
+	mov edi, 1	; Indica que o destino será em stdout
+	mov esi, msg	; pega o endereço do rótulo msg
+	mov edx, 14	; recebe o valor da quantidade de msg em bytes
+	syscall		; chama a syscall
 
-	mov eax, 60
-	xor edi, edi
+	mov eax, 60	; Armazena o valor da chamada de Exit
+	xor edi, edi	; zera o edi para informa o valor zero no código
 	syscall	
 
